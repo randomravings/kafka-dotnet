bbee40035d6c0d3c0f6b3fc407286b8cc81255b9
bergur ziska <bergur.ziska@gmail.com>
2023-12-21 15:23:20 +0100

    updated ssl environment

--- docker/cp-single-sasl_ssl/generate-cert.sh
+++ docker/cp-single-sasl_ssl/generate-cert.sh
@@ -5,68 +5,69 @@ L=Copenhagen
 CN=randomravings-ca
 
 # Create CA
-mkdir ca
-eval "echo \"$(< ca_template.cnf)\"" > ca/ca.cnf
+mkdir secrets
+mkdir secrets/ca
+eval "echo \"$(< ca_template.cnf)\"" > secrets/ca/ca.cnf
 openssl req -new -nodes \
    -x509 \
    -days 365 \
    -newkey rsa:2048 \
-   -keyout ca/ca.key \
-   -out ca/ca.crt \
-   -config ca/ca.cnf
-cat ca/ca.crt ca/ca.key > ca/ca.pem
+   -keyout secrets/ca/ca.key \
+   -out secrets/ca/ca.crt \
+   -config secrets/ca/ca.cnf
+cat secrets/ca/ca.crt secrets/ca/ca.key > secrets/ca/ca.pem
 
 for i in $(seq -f "%02g" 1 3);
 do
     BROKER="broker-${i}"
-    CN = ${BROKER}
+    CN=${BROKER}
     echo "------------------------------- ${BROKER} -------------------------------"
 
-   mkdir ${BROKER}-creds
-   eval "echo \"$(< broker_template.cnf)\"" > ${BROKER}-creds/${BROKER}.cnf
+   mkdir secrets/${BROKER}-creds
+   eval "echo \"$(< broker_template.cnf)\"" > secrets/${BROKER}-creds/${BROKER}.cnf
 
     # Create server key & certificate signing request(.csr file)
     openssl req -new \
     -newkey rsa:2048 \
-    -keyout ${BROKER}-creds/${BROKER}.key \
-    -out ${BROKER}-creds/${BROKER}.csr \
-    -config ${BROKER}-creds/${BROKER}.cnf \
+    -keyout secrets/${BROKER}-creds/${BROKER}.key \
+    -out secrets/${BROKER}-creds/${BROKER}.csr \
+    -config secrets/${BROKER}-creds/${BROKER}.cnf \
     -nodes
 
 
     # Sign server certificate with CA
     openssl x509 -req \
     -days 3650 \
-    -in ${BROKER}-creds/${BROKER}.csr \
-    -CA ca/ca.crt \
-    -CAkey ca/ca.key \
+    -in secrets/${BROKER}-creds/${BROKER}.csr \
+    -CA secrets/ca/ca.crt \
+    -CAkey secrets/ca/ca.key \
     -CAcreateserial \
-    -out ${BROKER}-creds/${BROKER}.crt \
-    -extfile ${BROKER}-creds/${BROKER}.cnf \
+    -out secrets/${BROKER}-creds/${BROKER}.crt \
+    -extfile secrets/${BROKER}-creds/${BROKER}.cnf \
     -extensions v3_req
 
     # Convert server certificate to pkcs12 format
     openssl pkcs12 -export \
-    -in ${BROKER}-creds/${BROKER}.crt \
-    -inkey ${BROKER}-creds/${BROKER}.key \
+    -in secrets/${BROKER}-creds/${BROKER}.crt \
+    -inkey secrets/${BROKER}-creds/${BROKER}.key \
     -chain \
-    -CAfile ca/ca.pem \
-    -name ${BROKER} \
-    -out ${BROKER}-creds/${BROKER}.p12 \
+    -CAfile secrets/ca/ca.pem \
+    -name secrets/${BROKER} \
+    -out secrets/${BROKER}-creds/${BROKER}.p12 \
     -password pass:secret
 
     # Create server keystore
     keytool -importkeystore \
-    -deststorepass confluent \
-    -destkeystore ${BROKER}-creds/kafka.${BROKER}.keystore.pkcs12 \
-    -srckeystore ${BROKER}-creds/${BROKER}.p12 \
+    -deststorepass secret \
+    -destkeystore secrets/${BROKER}-creds/kafka.${BROKER}.keystore.pkcs12 \
+    -srckeystore secrets/${BROKER}-creds/${BROKER}.p12 \
     -deststoretype PKCS12  \
     -srcstoretype PKCS12 \
     -noprompt \
     -srcstorepass secret
 
     # Save creds
-    echo "secret" > ${BROKER}-creds/${BROKER}_sslkey_creds
-    echo "secret" > ${BROKER}-creds/${BROKER}_keystore_creds
+    echo "secret" > secrets/${BROKER}-creds/${BROKER}_sslkey_creds
+    echo "secret" > secrets/${BROKER}-creds/${BROKER}_keystore_creds
 
 done
